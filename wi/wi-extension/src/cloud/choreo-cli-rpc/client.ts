3e70fad6b600d1367a31974d79d279f32a807e2e
kaje94 <a.kajendran@gmail.com>
2026-06-15 10:05:46 +0530

    Reconnect on closed connection in sendRequest method

--- wi/wi-extension/src/cloud/choreo-cli-rpc/client.ts
+++ wi/wi-extension/src/cloud/choreo-cli-rpc/client.ts
@@ -165,6 +165,7 @@ export class RPCClient {
 			ext.logError(`Error in RPC request: ${method} with params ${JSON.stringify(params)}`, e as Error);
 			// TODO: have a better way to check if connection is closed
 			if ((e.message?.includes("Connection is closed") || e.message?.includes(`Function ${method} timed out`)) && !isRetry) {
+				await this.init();
 				return this.sendRequest(method, params, timeout, true);
 			}
 			handlerError(e);
